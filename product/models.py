5ef9174d19b9fde56940978514da6e2adaa13eaf
Diminatorio <104588396+Diminatorio@users.noreply.github.com>
2023-03-13 13:51:15 +0200

    .

--- product/models.py
+++ product/models.py
@@ -5,19 +5,19 @@ from ckeditor.fields import RichTextField
 
 class CategoryModel(models.Model):
     photo = models.ImageField(upload_to='category/', null=True)
-    category_name = models.CharField(unique=True, max_length=15)
+    category_name = models.CharField(unique=True, max_length=40)
 
 
 class SubcategoryModel(models.Model):
     category_id = models.ForeignKey('CategoryModel', verbose_name="CATEGORY_ID", on_delete=models.DO_NOTHING)
-    subcategory_name = models.CharField(unique=True, max_length=20)
+    subcategory_name = models.CharField(unique=True, max_length=50)
     photo = models.ImageField(upload_to='subcategory/', null=True)
 
 
 class ProductModel(models.Model):
     subcategory_id = models.ForeignKey('SubcategoryModel', verbose_name="CATEGORY_ID", on_delete=models.DO_NOTHING)
     main_photo = models.ImageField(upload_to='product/', null=True)
-    product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=80)
     price = models.PositiveIntegerField()
     description = RichTextField(null=True, blank=True)
     code = models.CharField(max_length=50, default='SHK')
