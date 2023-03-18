ce3e555605fef865d5f5bdeb062b68ab19801a02
Diminatorio <104588396+Diminatorio@users.noreply.github.com>
2023-03-18 11:24:12 +0200

    .

--- CatalogProject/urls.py
+++ CatalogProject/urls.py
@@ -20,8 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('product.urls'))
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('product.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
